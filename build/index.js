b497dcc082446ddcdc1bad96bdd75031ad1aeb6c
youkidkk <youkidkk80@gmail.com>
2021-09-23 09:58:29 +0900

    build 配下を追加

---
+++ build/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const greet_1 = require("sample/greet");
+function default_1() {
+    console.log((0, greet_1.greet)("TypeScript"));
+}
+exports.default = default_1;
+//# sourceMappingURL=index.js.map
---
+++ build/sample/greet.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.greet = void 0;
+const greet = (name) => `Hello ${name}!`;
+exports.greet = greet;
+//# sourceMappingURL=greet.js.map
