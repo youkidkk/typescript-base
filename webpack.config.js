f7c970aee25400e3c45b137d1839a3a80b71a99c
youkidkk <youkidkk80@gmail.com>
2021-09-23 13:10:57 +0900

    webpack 用設定を追加

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "src/index.ts"),
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "index.js",
+  },
+  resolve: {
+    extensions: [".ts"],
+    alias: {
+      "#": path.resolve(__dirname, "src"),
+    },
+  },
+  target: "node",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader",
+          options: {
+            configFile: path.resolve(__dirname, "tsconfig-build.json"),
+          },
+        },
+      },
+    ],
+  },
+};
