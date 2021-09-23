bb79538d6470da7ab6b70fb3f58720ad9338bdef
youkidkk <youkidkk80@gmail.com>
2021-09-23 12:54:54 +0900

    build 配下を一旦削除

--- build/sample/greet.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.greet = void 0;
-const greet = (name) => `Hello ${name}!`;
-exports.greet = greet;
-//# sourceMappingURL=greet.js.map
