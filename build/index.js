994f952da8d8ba05a0a035603ba56ac75d5d2b17
youkidkk <youkidkk80@gmail.com>
2021-09-25 10:35:31 +0900

    リビルド

--- build/index.js
+++ build/index.js
@@ -1,79 +1,8 @@
-/*
- * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
- * This devtool is neither made for production nor for readable output files.
- * It uses "eval()" calls to create a separate source file in the browser devtools.
- * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
- * or disable the default devtool with "devtool: false".
- * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
- */
-(function webpackUniversalModuleDefinition(root, factory) {
-	if(typeof exports === 'object' && typeof module === 'object')
-		module.exports = factory();
-	else if(typeof define === 'function' && define.amd)
-		define([], factory);
-	else {
-		var a = factory();
-		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
-	}
-})(global, function() {
-return /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
-/******/ 	var __webpack_modules__ = ({
-
-/***/ "./src/index.ts":
-/*!**********************!*\
-  !*** ./src/index.ts ***!
-  \**********************/
-/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
-
-eval("\r\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\r\nconst greet_1 = __webpack_require__(/*! #/sample/greet */ \"./src/sample/greet.ts\");\r\nfunction default_1() {\r\n    console.log((0, greet_1.greet)(\"TypeScript\"));\r\n}\r\nexports[\"default\"] = default_1;\r\n\n\n//# sourceURL=webpack://typescript-base/./src/index.ts?");
-
-/***/ }),
-
-/***/ "./src/sample/greet.ts":
-/*!*****************************!*\
-  !*** ./src/sample/greet.ts ***!
-  \*****************************/
-/***/ ((__unused_webpack_module, exports) => {
-
-eval("\r\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\r\nexports.greet = void 0;\r\nconst greet = (name) => `Hello ${name}!`;\r\nexports.greet = greet;\r\n\n\n//# sourceURL=webpack://typescript-base/./src/sample/greet.ts?");
-
-/***/ })
-
-/******/ 	});
-/************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/ 	
-/************************************************************************/
-/******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.ts");
-/******/ 	
-/******/ 	return __webpack_exports__;
-/******/ })()
-;
-});
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const greet_1 = require("./sample/greet");
+function default_1() {
+    console.log((0, greet_1.greet)("TypeScript"));
+}
+exports.default = default_1;
+//# sourceMappingURL=index.js.map
